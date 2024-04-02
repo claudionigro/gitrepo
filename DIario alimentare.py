c95544765c867c0d080bfbfbc5098990b7e69886
claudio <cnigro@studenti.apuliadigitalmaker.it>
2024-04-02 09:52:00 +0200

    Ho aggiunto le classi Alimento e Pasto

--- DIario alimentare.py
+++ DIario alimentare.py
@@ -1,4 +1,20 @@
 import json
 from datetime import datetime, timedelta
 class Alimento:
+        def __init__(self, nome, calorie, proteine, carboidrati, grassi):
+        self.nome = nome
+        self.calorie = calorie
+        self.proteine = proteine
+        self.carboidrati = carboidrati
+        self.grassi = grassi
+class Pasto:
+    def __init__(self):
+        self.alimenti = []
+
+    def aggiungi_alimento(self, alimento):
+        self.alimenti.append(alimento)
+
+    def totale_calorie(self):
+        return sum(alimento.calorie for alimento in self.alimenti)
+        
 
