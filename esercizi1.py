f4363c2030ee7f815173beb726ca8e67f5ca1ba1
claudio <cnigro@studenti.apuliadigitalmaker.it>
2024-04-02 10:19:55 +0200

    ho eliminato file inutili dalla repo

--- esercizi1.py
+++
@@ -1,9 +0,0 @@
-variabileUno = input("inserisci un numero: ")
-print(variabileUno)
-variabileDue = 5
-variabileTre = input("Inserisci variabile 3")
-variabileTre = int(variabileTre)
-print(type(variabileTre))
-if variabileTre > variabileDue:
-    print("var3 maggiore")
-
